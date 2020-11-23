71f7d269f5d1d0b792ebe4c7526286d0b1198719
Lori-Alfred <74910068+Lori-Alfred@users.noreply.github.com>
2020-11-23 22:26:34 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+const hamburger1=document.querySelector('.hamburger  ');
+const firstNav1=document.querySelector('.firstnav ');
+
+
+hamburger1.addEventListener('click', function(){
+    firstNav1.classList.toggle('open');
+   
+});
+
+
+
+
+
