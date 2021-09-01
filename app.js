d37ce1021f2faa3e78557ec57c04c3ead2d363e9
Lori-Alfred <lorialfredg@gmail.com>
2021-09-01 09:00:49 +0100

    loader added

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
-const hamburger1=document.querySelector('.hamburger  ');
-const firstNav1=document.querySelector('.firstnav ');
+const hamburger1 = document.querySelector(".hamburger  ");
+const firstNav1 = document.querySelector(".firstnav ");
+const wrapper = document.querySelector(".wrapper ");
+const loader = document.querySelector(".loader ");
+console.log("wrapper:", wrapper);
+console.log("loader:", loader);
 
-
-hamburger1.addEventListener('click', function(){
-    firstNav1.classList.toggle('open');
-   
+hamburger1.addEventListener("click", function () {
+  firstNav1.classList.toggle("open");
 });
 
+function init() {
+  setTimeout(() => {
+    loader.style.opacity = "0";
+    loader.style.display = "none";
 
+    wrapper.style.display = "block";
+    setTimeout(() => {
+      wrapper.style.opacity = "1";
+    }, 50);
+  }, 4000);
+}
 
-
-
+init();
